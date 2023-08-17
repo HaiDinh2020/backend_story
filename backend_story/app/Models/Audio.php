12a25f07d8d56429107e3dba9288291d974cd8ad
HaiDinh2020 <haidinh6008@gmail.com>
2023-08-17 23:14:14 +0700

    logging with spatie and auth api with sanctum

--- backend_story/app/Models/Audio.php
+++ backend_story/app/Models/Audio.php
@@ -4,13 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Audio extends Model
 {
-    use HasFactory;
+    use HasFactory, LogsActivity, HasApiTokens;
     public $timestamps = false;
     protected $fillable = [
         'audio',
         'text_id'
     ];
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->setDescriptionForEvent(fn(string $eventName) => "This audio has been {$eventName}")
+            ->useLogName('Audio');
+    }
 }
